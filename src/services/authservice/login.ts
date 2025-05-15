5caa5e3b86b50847ab723dc40d157b833e05d177
gauravPanditr <gauravpandit7856@gmail.com>
2025-05-15 21:52:23 +0530

    implemeting login-service

--- src/services/authservice/login.ts
+++ src/services/authservice/login.ts
@@ -1,2 +1,61 @@
+import axios from "axios";
 
+export class LoginService {
+  constructor(
+    private baseUrl: string,
+    private projectId: string,
+    private projectKey: string
+  ) {}
 
+  private get headers() {
+    return {
+      "project-id": this.projectId,
+      "project-key": this.projectKey,
+    };
+  }
+
+  async passwordLogin(username: string, email: string, password: string) {
+    const response = await axios.post(
+      `${this.baseUrl}/user/auth/session/create`,
+      { username, email, password },
+      { headers: this.headers, withCredentials: true }
+    );
+    return response.data;
+  }
+
+  async initOTP(email: string) {
+    const response = await axios.post(
+      `${this.baseUrl}/user/auth/otp-on-email?initiate=true`,
+      { email },
+      { headers: this.headers, withCredentials: true }
+    );
+    return response.data;
+  }
+
+  async completeOTP(email: string, token: string) {
+    const response = await axios.post(
+      `${this.baseUrl}/user/auth/otp-on-email?otpToken=${token}`,
+      { email },
+      { headers: this.headers, withCredentials: true }
+    );
+    return response.data;
+  }
+
+  async initMagicURL(email: string, baseLink: string) {
+    const response = await axios.post(
+      `${this.baseUrl}/user/auth/magic-url?initiate=true`,
+      { email, baseLink },
+      { headers: this.headers, withCredentials: true }
+    );
+    return response.data;
+  }
+
+  async completeMagicURL(email: string, token: string) {
+    const response = await axios.post(
+      `${this.baseUrl}/user/auth/magic-url?magicURLToken=${token}`,
+      { email },
+      { headers: this.headers, withCredentials: true }
+    );
+    return response.data;
+  }
+}
