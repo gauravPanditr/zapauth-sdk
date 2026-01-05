b45c09060bbb9413e024a7d2e023b67082924449
gauravPanditr <gauravpandit7856@gmail.com>
2026-01-06 00:43:36 +0530

    fixing typescript issues

--- src/constants.ts
+++
@@ -1,3 +0,0 @@
-const API_VERSION = "v1";
-
-export const AUTHWAVE_API_BASE_URL = `https://api.authwave.in/api/${API_VERSION}`;
--- src/index.ts
+++ src/index.ts
@@ -1,3 +1,9 @@
-import { AuthService } from "./auth/auth.service";
 
-export { AuthService };
+import express,{Express} from "express";
+import serverConfig from "./config/serverConfig";
+const app :Express =express();
+app.listen(serverConfig.PORT,()=>{
+    console.log(`Server Started at *:${serverConfig.PORT}`);
+    
+    
+});
